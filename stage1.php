f7bb335c2678d8eb07d81d6a5c98eb8318c75f03
FUnsho Olaniyi <funsholaniyi@gmail.com>
2018-04-11 20:07:00 +0100

    first commit

---
+++ stage1.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Stage 1 Assigment</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+
+</head>
+<style>
+    body{
+        margin: 0;
+        padding: 0;
+        background-color: #ffffff;
+    }
+    .container{
+        margin: 0px 50px;
+        background-color: #EFEFEF;
+    }
+    header{
+        background-color: #367FA9;
+        height: 50px;
+        width: 100%;
+    }
+    main{
+        background-color: inherit;
+        min-height: 500px;
+    }
+    main .clock {
+        color: #404040;
+        margin: auto;
+        text-align: center;
+        padding: 150px;
+    }
+    .clock h2 {
+        font-family: cursive;
+        font-size: 70px;
+    }
+    footer{
+        background-color: #404040;
+        height: 70px;;
+    }
+</style>
+<body>
+        <header></header>    
+    <div class="container">
+            <main>
+                <div class="clock">
+                    <h2>07:19 PM</h2>
+                </div>
+            </main>
+    </div>
+    <footer></footer>
+    
+</body>
+</html>
